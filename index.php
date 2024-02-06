e6b45b643bc17f7ba37a86a00a754a4dc1f12270
valenfanelli <valentinafanelliaddiechi@hotmail.com>
2024-02-06 20:12:37 -0300

    subo urls actualizadas

--- index.php
+++ index.php
@@ -29,13 +29,13 @@
             include 'includes/templates/anuncios.php';
         ?>
         <div class="alinear-derecha">
-            <a href="anuncios.html" class="boton-verde">Ver todas</a>
+            <a href="anuncios.php" class="boton-verde">Ver todas</a>
         </div>
     </section>
     <section class="imagen-contacto">
         <h2>Encuentra la casa de tus sueños</h2>
         <p>Llena el formulario de contacto y un asesor se pondrá en contacto contigo a la brevedad</p>
-        <a href="contacto.html" class="boton-amarillo">Contactanos</a>
+        <a href="contacto.php" class="boton-amarillo">Contactanos</a>
     </section>
     <div class="contenedor seccion seccion-inferior">
         <section class="blog">
@@ -49,7 +49,7 @@
                     </picture>
                 </div>
                 <div class="texto-entrada">
-                    <a href="entrada.html">
+                    <a href="entrada.php">
                         <h4>Terraza en el techo de tu casa</h4>
                         <p class="informacion-meta">Escrito el: <span>22-01-2024</span> por: <span>Admin</span></p>
                         <p>Consejos para construir una terraza en el techo de tu casa con los mejores materiales a buen precio</p>
@@ -65,7 +65,7 @@
                     </picture>
                 </div>
                 <div class="texto-entrada">
-                    <a href="entrada.html">
+                    <a href="entrada.php">
                         <h4>Guia para la decoracion de tu hogar</h4>
                         <p class="informacion-meta">Escrito el: <span>22-01-2024</span> por: <span>Admin</span></p>
                         <p>Maximiza el espacio en tu hogar con esta guia, aprende a combinar muebles</p>
